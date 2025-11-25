0981232581109c64206647dbd244a4b76c50e674
ydemu <y.demuchan3860@gmail.com>
2025-11-25 15:55:41 +0900

    feat(generate_text): ✨ テキスト生成処理を更新し、argmaxを使用

--- gpt_model/generate_text.py
+++ gpt_model/generate_text.py
@@ -4,18 +4,19 @@ from gpt_model.gpt_model import GPTModel
 from gpt_model.config import GPT_CONFIG_124M
 
 def generate_text_simple(model,idx,max_new_tokens,context_size):
-    idx_cond = idx[:,-context_size:]
-    with torch.no_grad():
-        logits = model(idx_cond)
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:,-context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
 
-        #最後のステップのみに注目
-        logits = logits[:,-1,:]
+            #最後のステップのみに注目
+            logits = logits[:,-1,:]
 
-        probas = torch.softmax(logits,dim=-1)
+            probas = torch.softmax(logits,dim=-1)
 
-        idx_next = torch.argmax(probas,dim=-1,keepdim=True)
+            idx_next = torch.argmax(probas,dim=-1,keepdim=True)
 
-        idx = torch.cat((idx,idx_next),dim=1)
+            idx = torch.cat((idx,idx_next),dim=1)
     
     return idx
 
